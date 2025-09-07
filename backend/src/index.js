cdc64a6cf911b9dcaae1ba3def365b98a250c0ad
SAINATH6677 <patlollasainathreddy321@gmail.com>
2025-09-07 15:38:51 +0530

    finally completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import coockieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -9,13 +11,13 @@ import messageRoutes from "./routes/message.route.js";
 import { app,server } from "./lib/socket.js";
 
 dotenv.config();
-
-
-
 const PORT = process.env.PORT
 
+const __dirname = path.resolve();
+
+
 app.use(express.json());
-app.use(coockieParser());
+app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
@@ -23,6 +25,17 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));  
+
+
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    });
+}
+
+
 
 
 server.listen(PORT, () => {
